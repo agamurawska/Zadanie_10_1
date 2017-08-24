34d395173abbe4f3fe32991eb0de2826bdfb3f5f
aga <aga.murawska@gmail.com>
2017-08-24 22:55:16 +0200

    version 2.0

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 ﻿$(document).ready(function(){
-			//change of color of even spans
+		//change of color of even spans
 	$("span:even").css('color','red')
 
 		// new button to every paragraph
 	$("p").each(function(index, element){
 		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-		$(element).append(button)
-	})
+		$(element).append(button);
+
 		//pop up window on click on the button
-	button.click(function(){
-		alert($(this).attr("data-tmp"));
-	})
+		$("button").click(function(){
+			alert($(this).attr("data-tmp"));
+		})
+	})	
 });
